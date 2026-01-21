7269c909ab988f46595da4746fa8b946ac0fab9c
giovanni <giovanni_ge@hotmai.com>
2026-01-21 18:23:02 -0300

    feat

--- api-node-full/projeto/app.js
+++ api-node-full/projeto/app.js
@@ -1,8 +1,10 @@
 const http = require('http');
-const hanldle = require('./handle')
+const hanldle = require('./handle');
+const hello = require('console-log-hello-world');
+const PORT = 3000;
 
 const server = http.createServer(hanldle);
 
-server.listen(3000, () => {
-  console.log("olá mundo");
+server.listen(PORT, () => {
+  console.log(`A porta que está funcionando é ${PORT}`);
 });
