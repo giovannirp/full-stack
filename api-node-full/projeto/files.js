6e47583d4341cc4e22d77f86162a78f07c0ddfc9
giovanni <giovanni_ge@hotmai.com>
2026-01-21 15:53:21 -0300

    feat: add

--- api-node-full/projeto/files.js
+++ api-node-full/projeto/files.js
@@ -1,8 +1,36 @@
 const fs = require('fs');
 
 // Criando arquivo myfile.txt
-fs.writeFile('myfile.txt', 'Hello word from felipe Daniel', (err) => {
-  if(err) {
-    throw err;
-  }
-})
+// fs.writeFile('myfile.txt', 'Hello word from Giovanni Ribeiro', (err) => {
+//   if(err) {
+//     throw err;
+//   }
+// })
+
+// Ler arquivo
+// fs.readFile('myfile.txt', (err, data) => {
+//   if (err) {
+//     throw err;
+//   }
+
+//   console.log(data.toString('utf8'));
+// })
+
+// Ler diretório
+// fs.readdir('.', (err, files) => {
+//   if(err) {
+//     throw err;
+//   }
+//   for (var file in files) {
+//     console.log(files[file])
+//   }
+// });
+
+// Ler somente arquivo js
+fs.readdirSync('.')
+ .filter((file) => {
+    return (file.endsWith('.js'))
+ })
+ .forEach((file) => {
+    console.log(file);
+ })
