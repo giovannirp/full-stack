f71bf2d71162684ae195fa83a45838660aa491f3
giovanni <giovanni_ge@hotmai.com>
2026-01-27 12:17:21 -0300

    feat: add

--- api-node-full/projeto/routes/client.js
+++ api-node-full/projeto/routes/client.js
@@ -2,12 +2,9 @@ let express = require('express');
 let router = express.Router();
 
 router.get('/', (req, res) => {
-  res.send("Client Page")
+  res.render("client_list");
 });
 
-router.get('/welcome', (req, res) => {
-  res.send('Welcome to Client Page');
-});
 
 router.get('/new', (req, res) => {
   res.render('Client_new')
