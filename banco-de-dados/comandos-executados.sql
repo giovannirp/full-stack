8e29820f4d793e5d547155d14ae0d73254cef4d4
giovanni <giovanni_ge@hotmai.com>
2026-03-07 10:36:54 -0300

    feat: adicionando arquivos

---
+++ banco-de-dados/comandos-executados.sql
@@ -0,0 +1,16 @@
+/*SELECT * FROM TabelaClientes;*/
+
+
+/*
+Buscando campos especificos
+select nomecolaborador, cargo from TabelaColaboradores;
+*/
+
+/*
+Selecionando status que indica se o emprestimo está ativo
+select * from TabelaEmprestimo WHERE status = true;
+*/
+
+
+
+
